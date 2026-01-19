be115ae3b9e6567e199df4c9c4a1128c245010c1
yevhenyakubets <yevhen.yakubets@gmail.com>
2026-01-19 18:31:02 +0200

    Add mock champions items endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"status": "ok"}
+
+@app.get("/champion/{name}")
+def get_champion(name: str):
+    return {
+        "champion": name,
+        "radiant_items": ["Radiant Rabadon", "Radiant Guinsoo"],
+        "ornn_items": ["Infinity Force", "Death's Dance"]
+    }
