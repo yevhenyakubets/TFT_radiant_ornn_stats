bbdd2254155993d0d5026848b4191d3ef2ce4451
yevhenyakubets <yevhen.yakubets@gmail.com>
2026-01-22 16:56:35 +0200

    Added text field to search champion to frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,6 @@
 document.getElementById("load").addEventListener("click", async () => {
-  const res = await fetch("http://127.0.0.1:8000/champion/Ahri");
+  const name = document.getElementById("champion").value;
+  const res = await fetch(`http://127.0.0.1:8000/champion/${name}`);
   const data = await res.json();
   document.getElementById("output").textContent =
     JSON.stringify(data, null, 2);
