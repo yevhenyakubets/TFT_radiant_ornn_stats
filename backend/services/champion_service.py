69e0424ebd5a6769a65e91829ce756f8b556de9e
yevhenyakubets <yevhen.yakubets@gmail.com>
2026-01-22 17:54:41 +0200

    Added Riot API test request

--- backend/services/champion_service.py
+++
@@ -1,6 +0,0 @@
-def get_champion_data(name: str):
-    return {
-        "champion": name,
-        "radiant_items": ["Radiant Rabadon", "Radiant Guinsoo"],
-        "artifacts": ["Infinity Force", "Eternal Winter"]
-    }
