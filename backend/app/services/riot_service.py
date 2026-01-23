dcfa8c973aac148391c38bfaf2470f16205829d2
yevhenyakubets <yevhen.yakubets@gmail.com>
2026-01-23 18:15:15 +0200

    Extract units and items from single match

--- backend/app/services/riot_service.py
+++ backend/app/services/riot_service.py
@@ -8,3 +8,18 @@ def get_match(match_id: str):
     url = f"https://europe.api.riotgames.com/tft/match/v1/matches/{match_id}"
     r = requests.get(url, headers=headers)
     return r.json()
+
+def extract_units(match_json):
+    result = []
+
+    participants = match_json["info"]["participants"]
+
+    for player in participants:
+        for unit in player["units"]:
+            result.append({
+                "champion": unit["character_id"],
+                "items": unit["itemNames"],
+                "tier": unit["tier"]
+            })
+
+    return result
