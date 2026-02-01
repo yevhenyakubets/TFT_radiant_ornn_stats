77b25f2c33e4b3125130c75554cb24560ba32bdb
yevhenyakubets <yevhen.yakubets@gmail.com>
2026-02-01 21:48:58 +0200

    Added champions page with sample champions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import ChampionPage from "./pages/ChampionPage";
+import ChampionsListPage from "./pages/ChampionsListPage";
 
 function App() {
   return (
     <Routes>
+      <Route path="/champions" element={<ChampionsListPage />} />
       <Route path="/champions/:championName" element={<ChampionPage />} />
     </Routes>
   );
