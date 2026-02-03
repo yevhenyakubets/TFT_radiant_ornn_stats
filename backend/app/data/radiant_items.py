86ecb948fbf717ef2eaa667d2e4e296e9eff49ba
yevhenyakubets <yevhen.yakubets@gmail.com>
2026-02-03 21:19:29 +0200

    Changed artifact and radiant data so it can return dicts of respective items

--- backend/app/data/radiant_items.py
+++ backend/app/data/radiant_items.py
@@ -1,3 +1,6 @@
+import json
+from pathlib import Path
+
 RADIANT_ITEMS_BY_ROLE = {
     "tank": [
         "TFT5_Item_AdaptiveHelmRadiant",
@@ -129,3 +132,46 @@ RADIANT_ITEMS_BY_ROLE = {
         "TFT5_Item_StatikkShivRadiant",
     ],
 }
+
+ITEM_JSON_PATH = Path(__file__).parent / "tft-item.json"
+
+with open(ITEM_JSON_PATH, encoding="utf-8") as f:
+    raw = json.load(f)
+
+RADIANT_ITEMS: dict[str, dict] = {}
+
+def is_radiant_item(item_id: str) -> bool:
+    return "Radiant" in item_id
+
+# build all radiant items
+for entry in raw["data"].values():
+    item_id = entry["id"]
+
+    if not is_radiant_item(item_id):
+        continue
+
+    RADIANT_ITEMS[item_id] = {
+        "id": item_id,
+        "name": entry["name"],
+        "image": entry["image"]["full"],
+        "roles": []
+    }
+
+# layer roles
+for role, items in RADIANT_ITEMS_BY_ROLE.items():
+    for item_id in items:
+        if item_id in RADIANT_ITEMS:
+            RADIANT_ITEMS[item_id]["roles"].append(role)
+
+# remove artifacts with no assigned roles
+RADIANT_ITEMS = {
+    item_id: data
+    for item_id, data in RADIANT_ITEMS.items()
+    if data["roles"]
+}
+
+
+def get_all_radiant_items() -> dict[str, dict]:
+    return RADIANT_ITEMS
+
+print(len(RADIANT_ITEMS))
