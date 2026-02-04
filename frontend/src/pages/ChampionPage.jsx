1407f4cb24b37978309a21b5b7103448976a13b2
yevhenyakubets <yevhen.yakubets@gmail.com>
2026-02-04 14:57:51 +0200

    Changed routing for artifacts - artifacts/artifactdId instead of items/itemname

--- frontend/src/pages/ChampionPage.jsx
+++ frontend/src/pages/ChampionPage.jsx
@@ -2,25 +2,25 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function ChampionPage() {
-  const { championName } = useParams();
+  const { championId } = useParams();
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`http://127.0.0.1:8000/champions/${championName}`)
+    fetch(`http://127.0.0.1:8000/champions/${championId}`)
       .then(res => res.json())
       .then(json => {
         setData(json);
         setLoading(false);
       });
-  }, [championName]);
+  }, [championId]);
 
   if (loading) return <div>Loading...</div>;
   if (data.error) return <div>{data.error}</div>;
 
   return (
     <div style={{ padding: "20px" }}>
-      <h1>{championName}</h1>
+      <h1>{data.name}</h1>
 
       <h2>Artifacts</h2>
       <table border="1" cellPadding="6">
@@ -32,7 +32,7 @@ function ChampionPage() {
           </tr>
         </thead>
         <tbody>
-          {Object.entries(data.artifact).map(([item, info]) => (
+          {Object.entries(data.artifacts).map(([item, info]) => (
             <tr key={item}>
               <td>{info.name}</td>
               <td>{info.count}</td>
@@ -52,7 +52,7 @@ function ChampionPage() {
           </tr>
         </thead>
         <tbody>
-          {Object.entries(data.radiant).map(([item, info]) => (
+          {Object.entries(data.radiants).map(([item, info]) => (
             <tr key={item}>
               <td>{info.name}</td>
               <td>{info.count}</td>
