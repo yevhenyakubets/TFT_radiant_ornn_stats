3050034d6da9c30d8ae3c2685052d6d81207712f
yevhenyakubets <yevhen.yakubets@gmail.com>
2026-02-14 19:15:48 +0200

    Added two new fields to the return of specific item and champion endpoints - valid and low sample

---
+++ backend/app/models/champion_item_valid_pairs.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.sql import func
+from app.database import Base
+
+class ChampionItemValidPairs(Base):
+    __tablename__ = "champion_item_valid_pairs"
+
+    id = Column(Integer, primary_key=True)
+
+    champion_id = Column(Integer, ForeignKey("champions.id"), primary_key=True)
+    item_id = Column(Integer, ForeignKey("items.id"), primary_key=True)
