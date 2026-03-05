722df32bf8720685687a586768c086f4156252f4
yevhenyakubets <yevhen.yakubets@gmail.com>
2026-03-05 15:55:05 +0200

    Deleted scripts that are no longer necessary

--- backend/app/scripts/create_stats_table.py
+++
@@ -1,9 +0,0 @@
-from app.database import engine
-from app.models.champion_item_stats import ChampionItemStats
-
-def run():
-    ChampionItemStats.__table__.create(bind=engine, checkfirst=True)
-    print("champion_item_stats table created.")
-
-if __name__ == "__main__":
-    run()
