a249a335f746367aac6e8b20a166f70e7d5d41cc
yevhenyakubets <yevhen.yakubets@gmail.com>
2026-03-06 21:37:10 +0200

    Added example file for database ingestion pipeline

---
+++ run_pipeline_example.bat
@@ -0,0 +1,12 @@
+@echo off
+cd /d C:\path\to\your\project\backend
+call venv\Scripts\activate
+set PYTHONPATH=C:\path\to\your\project\backend
+python -m backend.app.scripts.riot_ingest --tier diamond --division I
+python -m backend.app.scripts.riot_ingest --tier diamond --division II
+python -m backend.app.scripts.riot_ingest --tier diamond --division III
+python -m backend.app.scripts.riot_ingest --tier diamond --division IV
+python -m backend.app.scripts.riot_ingest --tier master
+python -m backend.app.scripts.riot_ingest --tier grandmaster
+python -m backend.app.scripts.riot_ingest --tier challenger
+python -m backend.app.scripts.populate_stats_table
