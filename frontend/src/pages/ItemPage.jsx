f9c257f6dbd2989cafcaad1d03aff3b3ccead897
yevhenyakubets <yevhen.yakubets@gmail.com>
2026-03-10 17:41:05 +0200

    Deleted unused import from itempage

--- frontend/src/pages/ItemPage.jsx
+++ frontend/src/pages/ItemPage.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router-dom";
-import Tooltip from "../components/Tooltip";
 import "../styles/common.css";
 import "../styles/ItemPage.css";
 
