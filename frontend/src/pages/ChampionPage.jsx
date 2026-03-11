7a7e790cf201a7a7af51a12e88434d8e1eb14106
yevhenyakubets <yevhen.yakubets@gmail.com>
2026-03-11 17:39:26 +0200

    Changed colors of cards on champion page to match those in champion list page

--- frontend/src/pages/ChampionPage.jsx
+++ frontend/src/pages/ChampionPage.jsx
@@ -131,12 +131,12 @@ function ChampionPage() {
 
   const getRarityColor = (cost) => {
     switch (cost) {
-      case 1: return "#808080";
-      case 2: return "#11b288";
-      case 3: return "#207ac7";
-      case 4: return "#c440da";
+      case 1: return "rgb(24, 36, 49)";
+      case 2: return "rgb(20, 77, 29)";
+      case 3: return "rgb(28, 52, 93)";
+      case 4: return "rgb(102, 20, 79)";
       case 5:
-      case 7: return "#ffb93b";
+      case 7: return "rgb(134, 84, 11)";
       default: return "#ccc";
     }
   };
