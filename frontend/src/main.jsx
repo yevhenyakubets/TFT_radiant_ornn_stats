fc845d0af33dd35d81f7c34e8b49e5ac1463d336
yevhenyakubets <yevhen.yakubets@gmail.com>
2026-03-11 17:06:57 +0200

    Lift search data fetching into shared context to prevent duplicate API calls

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/index.css"
+import { SearchDataProvider } from "./context/SearchDataContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <SearchDataProvider>
+      <App />
+    </SearchDataProvider>
   </BrowserRouter>
 );
