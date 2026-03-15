e03bc75f70074d39a93004c2a7e1ee89d6b41b8a
yevhenyakubets <yevhen.yakubets@gmail.com>
2026-03-15 16:04:01 +0200

    Created model for a new table in database

---
+++ backend/app/models/champion_stats.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, String, Integer
+from app.database import Base
+
+class ChampionStat(Base):
+    __tablename__ = "champion_stats"
+
+    champion_id = Column(String, primary_key=True)
+    match_id = Column(String, primary_key=True)
+    patch = Column(String)
+    placement = Column(Integer)
