8a08789b88f08ec5fe407e0e5aad360b92ac7db1
flameless <flamelessowo@gmail.com>
2026-03-15 15:49:02 +0100

    global project enhancements vol 1

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
-DATABASE_URL = "postgresql://postgres:081215kekskrtPGS@localhost:5432/tft_stats"
+DATABASE_URL = "postgresql://postgres:081215kekskrtPGS@localhost:5432/tft_stats" # TODO: should be envs
 
 engine = create_engine(DATABASE_URL)
 
